d16f98d87c8481403e31adf9476945d4c5ed82ac
Collin <williamsck233@gmail.com>
2025-09-17 12:35:29 -0400

    Added a function that finds a random operation using a list and implemented it into max score function

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const prompt = require('prompt-sync')()
 console.log(`Press 1 to play Max Score`);
 console.log(`Press 2 to play Three-Out`);
 let option = Number(prompt(`Enter option: `))
-
+let num1 = Math.floor(Math.random() * 11)
+let num2 = Math.floor(Math.random() * 11)
+let ans = 0;
 do{
     if(isNaN(option) || option > 2 ){
         console.log(`Invalid Answer`);
@@ -17,27 +19,46 @@ do{
         break;
     }
 }while(true)
+function randomOp(){
+    let symbols = [`+`, `-`, `*`, `/`]
+    let pickRandom = Math.floor(Math.random() * symbols.length);
+    return symbols[pickRandom];
+}   
 
 function maxScore(){
     
     let score = 0; 
     for(let i = 1; i <= 20; i++){
-        let num1 = Math.floor(Math.random() * 11)
-        let num2 = Math.floor(Math.random() * 11)
-        console.log(`${num1} + ${num2} = `)
-        let ans = Number(prompt(`Enter Answer: `)) 
-        if(isNaN(ans)){
-            score = score - 0;
-            console.log(`Okay, Your score: ${score}`);
-        }else if(ans == (num1 + num2) ){
-            score = score + 10;
-            console.log(`Correct! your score: ${score}`)
-            
-        }else if(ans !== (num1 + num2))
-        {
-            score = score - 5;
-            console.log(`Wrong! your score: ${score}`)
+        num1 = Math.floor(Math.random() * 11)
+        num2 = Math.floor(Math.random() * 11)
+        let pickedOp = randomOp();
+        if(pickedOp == `+`){
+            console.log(`${num1} + ${num2} = `);
+            ans = Number(prompt(`Enter Answer: `))
+        }else if(pickedOp == `-`){
+            console.log(`${num1} - ${num2} = `);
+            ans = Number(prompt(`Enter Answer: `))
+        }else if(pickedOp == `*`){
+            console.log(`${num1} * ${num2} = `);
+            ans = Number(prompt(`Enter Answer: `))
+        }else if(pickedOp == `/`){
+            console.log(`${num1} / ${num2} = `);
+            ans = Number(prompt(`Enter Answer: `))
         }
+        // console.log(`${num1} + ${num2} = `)
+        // let ans = Number(prompt(`Enter Answer: `)) 
+        // if(isNaN(ans)){
+        //     score = score - 0;
+        //     console.log(`Okay, Your score: ${score}`);
+        // }else if(ans == (num1 + num2) ){
+        //     score = score + 10;
+        //     console.log(`Correct! your score: ${score}`)
+            
+        // }else if(ans !== (num1 + num2))
+        // {
+        //     score = score - 5;
+        //     console.log(`Wrong! your score: ${score}`)
+        // }
     }
 }
 
@@ -54,21 +75,20 @@ function threeOut(){
         let ans = Number(prompt(`Enter Answer: `)) 
         if(isNaN(ans)){
             score = score - 0;
-            console.log(`Okay, Your score: ${score}`);
+            console.log(`Okay, Your score: ${score}, Lives left: ${lives}`);
         }else if(ans == (num1 + num2) ){
             score = score + 10;
-            console.log(`Correct! your score: ${score}`)
+            console.log(`Correct! your score: ${score}, Lives left: ${lives}`)
             
         }else if(ans !== (num1 + num2))
         {
             lives = lives - 1;
-            console.log(`Wrong! Lives left: ${lives}`)
+            console.log(`Wrong! your score: ${score}, Lives left: ${lives}`)
             if(lives == 0){
                 console.log(`${score}`)
                 return;
             }
         }
-        
     }
     for(x = 1; x <= 5; x++){
         num1 = Math.floor(Math.random() * 11)
